890d019cca6cc087335b7608b9b0f066cb49d3b0
tomjudd1 <tom@animade.tv>
2016-04-20 16:56:29 +0100

    Tapered stroke does TOGGLE q
    TOGGLE LAYERS ON AND OFF

--- 3-animade_taperedStroke/animade_taperedStroke.jsx
+++ 3-animade_taperedStroke/animade_taperedStroke.jsx
@@ -27,53 +27,139 @@
 var anmd_taper_Data = new Object();	// Store globals in an object
 
 // Set the version
-anmd_taper_Data.scriptName = 'Animade Tools - Onion Skin';
+anmd_taper_Data.scriptName = 'Animade Tools - Taper';
 anmd_taper_Data.version = '1.0.0';
 
 writeLn(anmd_taper_Data.scriptName + " - " + anmd_taper_Data.version);
 
 anmd_taper_Data.comp = app.project.activeItem;
 anmd_taper_Data.onOff = true;
+
+// Expressions
 anmd_taper_Data.ex_1 = "";
-anmd_taper_Data.layerName = "Onion Skin";
+
+// Names
+anmd_taper_Data.taperGroupName = "Animade Taper";
+
+// Errors
+anmd_taper_Data.noPathSel = "Select a path to taper";
+anmd_taper_Data.tapersOn = "Tapers turned off";
+anmd_taper_Data.tapersOff = "Tapers turned on";
+
 anmd_taper_Data.exists = false;
+
+anmd_taper_Data.taperLayers = [];
 
 anmd_taper_Data.anmd_taper_stepsForward = 1;
 anmd_taper_Data.anmd_taper_stepsForward = 1;
 
 if (anmd_taper_Data.comp){
 
-	// Is there an onion layer target layer
-	var allLayers = anmd_taper_Data.comp.layers;
-	var onionLayer;
-	var steps;
+	app.beginUndoGroup("Run Animade Taper");
+		
 
-	// Look for onion Layer
-	for (var i=1; i <= allLayers.length; i++) {
-		if (allLayers[i].name == anmd_taper_Data.layerName) {
+
+	// 1 - If a path is selected then add a tracking Null - else if it already has one then reconnect it along with any others in the comp
+	if (anmd_taper_Data.comp.selectedProperties[0]) {
+
+	}
+	// 2 - Else if no path selected look for taper layers and toggle on / off
+	else
+	{
+	
+		// Look for taper layer and add to taperLayers array
+		var allLayers = anmd_taper_Data.comp.layers;
+		for (var i=1; i <= allLayers.length; i++) {
+			if (allLayers[i].comment == 'taper') {
+				anmd_taper_Data.taperLayers.push(allLayers[i]);
+			}
+		}		
+		
+		if(anmd_taper_Data.taperLayers.length > 0){
 			anmd_taper_Data.exists = true;
-			onionLayer = allLayers[i];
-			break;
 		}else{
 			anmd_taper_Data.exists = false;
 		}
-	}
 
-	// Create if it does not exist
-	if(!anmd_taper_Data.exists){
-		onionLayer = anmd_taper_Data.comp.layers.addSolid([240,240,90], "Onion Skin", anmd_taper_Data.comp.width, anmd_taper_Data.comp.height,1);;
-		onionLayer.name = anmd_taper_Data.layerName;
-		onionLayer.startTime = 0;
-		onionLayer.adjustmentLayer = true;
-		onionLayer.locked = false;	
-		onionLayer.label = 2;	
-		onionLayer.shy = true;	
-		onionLayer.property("Effects").addProperty("CC Wide Time");
-		allLayers = anmd_taper_Data.comp.layers;
-		onionLayer.moveBefore(allLayers[1]);
-	}else{
-		onionLayer.enabled = !onionLayer.enabled;
-		onionLayer.moveBefore(allLayers[1]);
+		// 2.1 if taper layer found
+		if(anmd_taper_Data.exists == true){
+			
+			// on or off? Get from first in array
+			anmd_taper_Data.onOff = anmd_taper_Data.taperLayers[0].enabled;
+
+			var taperedLayers = anmd_taper_Data.taperLayers;
+			for (var i=0; i < taperedLayers.length; i++) {
+
+				// 2.1.1 - If tapers off then turn on
+				if(anmd_taper_Data.onOff == true){	
+					anmd_taper_Data.taperLayers[i].enabled = false;
+					writeLn(anmd_taper_Data.tapersOn);
+				}
+				// 2.1.2 - Else if tapers on then turn off
+				else{
+					anmd_taper_Data.taperLayers[i].enabled = true;
+					writeLn(anmd_taper_Data.tapersOff);
+				}	
+			
+			}
+
+		}
+		// 2.2 else if no taper layer found
+		else
+		{
+			writeLn(anmd_taper_Data.noPathSel);
+		}
 	}
+}
+
+function lookforPropertyAndReturnInArray(arrayToSearch, searchTerm){
+
+	var a = [1,1];
+	
+	for (var i=1; i <= arrayToSearch.length; i++) {
+		propertyName = arrayToSearch[i].name;
+		results = propertyName.indexOf(searchTerm);
+		//writeLn(layerName + " - " + i);
+
+		if (results > -1) {
+			
+			a.push(arrayToSearch[i])
+
+		}
+	}	
+
+	return a;
 
 }
+
+// function anmd_taper_findTrackingNulls(){
+	
+// 	var allLayers = anmd_taper_Data.comp.layers;
+// 	var layerName;
+// 	var results;
+
+// 	// Look for shape Layer
+// 	for (var i=1; i <= allLayers.length-1; i++) {
+
+// 		// continue if this is not a shape layer
+// 		if (allLayers[i].type) {}
+
+// 		layerName = allLayers[i].name;
+// 		results = layerName.indexOf(anmd_taper_Data.taperGroupName);
+// 		//writeLn(layerName + " - " + i);
+
+// 		if (results > -1) {
+			
+// 			anmd_taper_Data.taperLayers.push(allLayers[i])
+
+// 		}
+
+// 	}
+
+// 	if(anmd_taper_Data.trackerLayers.length > 0){
+// 		return true;
+// 	}else{
+// 		return false;
+// 	}
+
+// }
